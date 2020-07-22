1cf3ee726f01542ba7989981b71347bcb962da93
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-07-22 14:41:36 +0700

    init

---
+++ src/script.js
@@ -0,0 +1,32 @@
+const requestApi = async () => {
+  try {
+    const req = await fetch(
+      "https://natours-raihan.herokuapp.com/api/v1/tours"
+    );
+    const res = await req.json();
+    const target = document.getElementById("result");
+
+    res.data.data.forEach((tours) => {
+      target.innerHTML = `
+            <div class="card">
+                <img
+                src="https://natours-raihan.herokuapp.com/img/tours/${tours.images[0]}"
+                class="card-img-top"
+                alt="..."
+                />
+            <div class="card-body">
+              <h5 class="card-title">${tours.name}</h5>
+              <p class="card-text">
+                ${tours}
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        `;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+requestApi();
